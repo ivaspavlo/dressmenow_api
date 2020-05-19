1498a2e64ce2ee9fc8493f3ef3b70032fd606205
pavel-ivashchenko <pavel.ivashchenko@itrexgroup.com>
2020-05-19 11:39:45 +0300

    feat(db): implemented db connection

--- server.js
+++ server.js
@@ -3,26 +3,38 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error.mware');
+
 
 // LOAD ENV VARS
 dotenv.config({ path: './config/config.env' });
 
+
+// CONNECT TO DATABASE
+connectDB();
+
+
 const app = express();
+
 
 // DEV LOGGING MIDDLEWARE
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
+
 // MOUNT ROUTES
 app.use(errorHandler);
 
+
 const PORT = process.env.PORT || 5000;
+
 
 const server = app.listen(
   PORT, console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`.yellow.bold)
 );
+
 
 // HANDLE UNHANDLED PROMISE REJECTIONS
 process.on('unhandledRejection', (err, _) => {
